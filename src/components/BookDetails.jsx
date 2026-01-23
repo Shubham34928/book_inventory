39cbd44993df84ea587bdb8ac57a98a80ec58f02
ShubhamBachane <bachaneshubham3492@gmail.com>
2026-01-23 18:38:12 +0530

    Added back button to bookdetails page to navigate back to homepage

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,7 +1,11 @@
 import { useLocation } from 'react-router-dom'
+import { useNavigate} from 'react-router-dom'
+
 
 function BookDetails() {
   const { state } = useLocation()
+  const navigate = useNavigate()
+
   const book = state?.book
 
   if (!book) {
@@ -10,13 +14,19 @@ function BookDetails() {
 
   return (
     <div>
+        
+      <button onClick={() => navigate("/")}>Back to Home</button> 
+      <br/>
       {book.image && (<img src={book.image} alt={book.title} width="200" />)}
+      
       <h2>{book.title}</h2>
       <p><strong>Author:</strong> {book.author}</p>
       <p><strong>Publisher:</strong> {book.publisher}</p>
       <p><strong>Published Date:</strong> {book.publishedDate}</p>
       <p><strong>Description:</strong>{book.description}</p>
       <p>{book.description}</p>
+      
+
 
     </div>
   )
